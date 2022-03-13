14d9a0f25c5297c8a46a5fbd51be7aceaa3a02dc
Yi Ming <ofseed@foxmail.com>
2022-03-13 23:27:23 +0800

    Extract the neovim configuration file

--- nvim/lua/config/interface/nvim-colorizer.lua
+++
@@ -1,7 +0,0 @@
-local ok, colorizer = pcall(require, "colorizer")
-if not ok then
-  vim.notify "Could not load colorizer"
-  return
-end
-
-colorizer.setup {}
--- nvim/lua/config/tools/nabla.lua
+++
@@ -1,8 +0,0 @@
-local vim = vim
-local ok, nabla = pcall(require, "nabla")
-if not ok then
-  vim.notify "Could not load nabla"
-  return
-end
-
-vim.api.nvim_set_keymap("n", "<leader>p", ":lua require'nabla'.popup()<CR>", { noremap = true })
